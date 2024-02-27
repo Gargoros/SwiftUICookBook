f02395f0aae4fb3aefb15d756ee06ef9e26b2d1e
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-02-27 18:01:30 +0300

    Initial Commit

---
+++ SwiftUICookBook/SwiftUICookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICookBookApp.swift
+//  SwiftUICookBook
+//
+//  Created by MIKHAIL ZHACHKO on 27.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
